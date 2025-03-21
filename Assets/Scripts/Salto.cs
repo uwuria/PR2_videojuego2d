015bba736c8af588285e3f85f7729bfc0f0abc3d
“uwuria” <mpermur1@upv.edu.es>
2025-03-21 13:48:38 +0100

    first commit

---
+++ Assets/Scripts/Salto.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salto : MonoBehaviour
+{
+
+    public int miEdad = 21;
+
+    float miAltura = 4.0f; 
+
+    string miNombre = "uwu";
+
+    bool azucarero = false; 
+
+
+    public GameObject miCubo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //uwu
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Debug.Log("Saludos Caballero. Mi nombre es "+miNombre+" y tengo "+miEdad+" años");
+
+    }
+}
